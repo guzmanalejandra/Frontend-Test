07698c06410518295146d5a6aa7f24b78eba0834
guzmanalejandra <77361246+guzmanalejandra@users.noreply.github.com>
2023-08-30 12:15:25 -0600

    feat(): React-Window

--- Frontend-Test/src/App.tsx
+++ Frontend-Test/src/App.tsx
@@ -9,11 +9,9 @@ import arrowup from './assets/arrow-up/arrowup.svg';
 import mediumfilledheart from './assets/medium-filled-heart/mediumfilledheart.svg';
 import ContentLoader from 'react-content-loader';
 import axios from 'axios';
+import { FixedSizeGrid as Grid } from "react-window";
 
-
-
-// Componente para mostrar la información de un superhéroe
-function HeroBox({ hero, toggleLikeFunc, isLiked, recentlyLikedHeroId }) {  // Agregamos recentlyLikedHeroId como prop
+function HeroBox({ hero, toggleLikeFunc, isLiked, recentlyLikedHeroId }) {
     const averageScore = (
         hero.powerstats.intelligence + 
         hero.powerstats.strength + 
@@ -24,9 +22,7 @@ function HeroBox({ hero, toggleLikeFunc, isLiked, recentlyLikedHeroId }) {  // A
     return (
         <div className="hero-item">
             <div className="hero-box">
-                
                 {hero.id === recentlyLikedHeroId && <div className="recently-liked">Recently Liked</div>}
-
                 <div className="hero-background" style={{ backgroundImage: `url(${hero.images.md})` }}></div>
                 <img src={hero.images.md} alt={hero.name} className="hero-image" />
                 <div className="liked-icon-container" onClick={() => toggleLikeFunc(hero)}>
@@ -45,44 +41,42 @@ function HeroBox({ hero, toggleLikeFunc, isLiked, recentlyLikedHeroId }) {  // A
     );
 }
 
+function useWindowDimensions() {
+    const [dimensions, setDimensions] = useState({ width: window.innerWidth, height: window.innerHeight });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setDimensions({ width: window.innerWidth, height: window.innerHeight });
+        };
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return dimensions;
+}
+
 function App() {
-    // Estado de la aplicación
-    const [loading, setLoading] = useState(true); // Estado de carga de datos
-    const [heroes, setHeroes] = useState<any[]>([]); // Lista de héroes
-    const [likedHeroes, setLikedHeroes] = useState<any[]>([]); // Héroes que el usuario ha dado "me gusta"
-    const [isLikedExpanded, setIsLikedExpanded] = useState(false); // Expandir o contraer la sección de "me gusta"
-    const [searchTerm, setSearchTerm] = useState(""); // Término de búsqueda
-    const [noHeroesFound, setNoHeroesFound] = useState(false);  // Indicador de no hay héroes encontrados
-    const [searchChanged, setSearchChanged] = useState(false); // Indicador de que el término de búsqueda ha cambiado
-    const [toggleAnimation, setToggleAnimation] = useState(false); // Alternar animación al cambiar la búsqueda
+    const [loading, setLoading] = useState(true);
+    const [heroes, setHeroes] = useState<any[]>([]);
+    const [likedHeroes, setLikedHeroes] = useState<any[]>([]);
+    const [isLikedExpanded, setIsLikedExpanded] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [noHeroesFound, setNoHeroesFound] = useState(false);
+    const [searchChanged, setSearchChanged] = useState(false);
+    const [toggleAnimation, setToggleAnimation] = useState(false);
     const [recentlyLikedHeroId, setRecentlyLikedHeroId] = useState<number | null>(null);
+    const { width } = useWindowDimensions();
 
-
-
-    // Filtrar héroes según el término de búsqueda
     const filteredHeroes = heroes.filter(hero => 
         hero.name.toLowerCase().includes(searchTerm.toLowerCase()) || 
         hero.biography.fullName.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
-    const heroesToDisplay = filteredHeroes;
-
-    // Efecto para cargar los héroes al montar el componente
     useEffect(() => {
         async function fetchData() {
             try {
                 const response = await axios.get('https://akabab.github.io/superhero-api/api/all.json');
-                const savedLikedHeroes = localStorage.getItem('likedHeroes');
-                console.log('Retrieved liked heroes from localStorage:', savedLikedHeroes);
-
-                 // Si hay héroes guardados en localStorage que el usuario ha dado "me gusta", se filtran
-                if (savedLikedHeroes) {
-                    const parsedLikedHeroes = JSON.parse(savedLikedHeroes);
-                    setLikedHeroes(parsedLikedHeroes);
-                    setHeroes(response.data.filter(hero => !parsedLikedHeroes.some(likedHero => likedHero.id === hero.id)));
-                } else {
-                    setHeroes(response.data);
-                }
+                setHeroes(response.data);
             } catch (error) {
                 console.error("Error fetching data:", error);
             } finally {
@@ -91,47 +85,35 @@ function App() {
         }
         fetchData();
     }, []);
-
-     // Efecto para verificar si no se encontraron héroes
+    
     useEffect(() => {
-        setNoHeroesFound(heroesToDisplay.length === 0); 
-    }, [heroesToDisplay]);
+        const savedLikedHeroIds = localStorage.getItem('likedHeroIds');
+        if (savedLikedHeroIds) {
+            const parsedLikedHeroIds = JSON.parse(savedLikedHeroIds);
+            setLikedHeroes(parsedLikedHeroIds);
+        }
+    }, []);
 
-    // Efecto para guardar héroes con "me gusta" en localStorage
     useEffect(() => {
-        console.log('Saving liked heroes to localStorage:', likedHeroes);
-        localStorage.setItem('likedHeroes', JSON.stringify(likedHeroes));
+        setNoHeroesFound(filteredHeroes.length === 0); 
+    }, [filteredHeroes]);
+
+    useEffect(() => {
+        const likedHeroIds = likedHeroes.map(hero => hero.id);
+        localStorage.setItem('likedHeroIds', JSON.stringify(likedHeroIds));
     }, [likedHeroes]);
 
-    // Verificar si un héroe tiene "me gusta"
     function isHeroLiked(heroId: number) {
         return likedHeroes.some(h => h.id === heroId);
     }
 
     function toggleLike(hero: any) {
         if (isHeroLiked(hero.id)) {
-            setLikedHeroes(prev => {
-                const updated = prev.filter(h => h.id !== hero.id);
-                console.log('Liked Heroes after unliking:', updated);
-                return updated;
-            });
-            setHeroes(prev => {
-                const updated = [...prev, hero];
-                console.log('All Heroes after unliking:', updated);
-                return updated;
-            });
-        // Si no tiene "me gusta", se añade
+            setLikedHeroes(prev => prev.filter(h => h.id !== hero.id));
+            setHeroes(prev => [...prev, hero]);
         } else {
-            setLikedHeroes(prev => {
-                const updated = [...prev, hero];
-                console.log('Liked Heroes after liking:', updated);
-                return updated;
-            });
-            setHeroes(prev => {
-                const updated = prev.filter(h => h.id !== hero.id);
-                console.log('All Heroes after liking:', updated);
-                return updated;
-            });
+            setLikedHeroes(prev => [...prev, hero]);
+            setHeroes(prev => prev.filter(h => h.id !== hero.id));
             setRecentlyLikedHeroId(hero.id);
             setTimeout(() => {
                 setRecentlyLikedHeroId(null);
@@ -139,7 +121,6 @@ function App() {
         }
     }
 
-    // Renderizado del componente principal
     return (
         <div className="App">
             <div className="logo-container">
@@ -155,7 +136,6 @@ function App() {
                     <div className={`liked-content ${isLikedExpanded ? 'expanded' : ''}`}>
                         {likedHeroes.map(hero => (
                             <HeroBox key={hero.id} hero={hero} toggleLikeFunc={toggleLike} isLiked={isHeroLiked(hero.id)} recentlyLikedHeroId={recentlyLikedHeroId} />
-
                         ))}
                     </div>
                 )}
@@ -183,10 +163,11 @@ function App() {
                                 type="text" 
                                 placeholder="Search" 
                                 value={searchTerm} 
-                                onChange={e => {setSearchTerm(e.target.value);
-                                                setSearchChanged(true);
-                                                setToggleAnimation(prev => !prev);
-                                                }}
+                                onChange={e => {
+                                    setSearchTerm(e.target.value);
+                                    setSearchChanged(true);
+                                    setToggleAnimation(prev => !prev);
+                                }}
                                 className="search-input"
                             />
                             <img src={cancel} alt="Cancel Search" className="cancel-icon" onClick={() => setSearchTerm('')} />
@@ -194,11 +175,30 @@ function App() {
                         {noHeroesFound ? (
                             <div className="no-heroes-found">No heroes found</div>
                         ) : (
-                            <div className={`hero-row ${searchChanged ? (toggleAnimation ? 'fade-in' : 'fade-in-b') : ''}`} onAnimationEnd={() => setSearchChanged(false)}>
-                                {heroesToDisplay.map(hero => (
-                                    <HeroBox key={hero.id} hero={hero} toggleLikeFunc={toggleLike} isLiked={isHeroLiked(hero.id)} />
-                                ))}
-                            </div>
+                            <Grid
+                                className={`hero-grid ${searchChanged ? (toggleAnimation ? 'fade-in' : 'fade-in-b') : ''}`}
+                                columnCount={5}
+                                columnWidth={230}
+                                height={500}
+                                rowCount={Math.ceil(filteredHeroes.length / 5)}
+                                rowHeight={150}
+                                width={width}
+                            >
+                                {({ columnIndex, rowIndex, style }) => {
+                                    const heroIndex = rowIndex * 5 + columnIndex;
+                                    if (heroIndex >= filteredHeroes.length) return <div style={style}></div>;
+                                    return (
+                                        <div style={style}>
+                                            <HeroBox 
+                                                key={filteredHeroes[heroIndex].id} 
+                                                hero={filteredHeroes[heroIndex]} 
+                                                toggleLikeFunc={toggleLike} 
+                                                isLiked={isHeroLiked(filteredHeroes[heroIndex].id)} 
+                                            />
+                                        </div>
+                                    );
+                                }}
+                            </Grid>
                         )}
                     </>
                 )}
